86d151eb119dc56475262591916797645c6f21b5
Peavy101 <matthewhardy709@gmail.com>
2024-05-10 09:08:38 -0230

    WIP

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+// import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import Root, { 
+  loader as rootLoader, 
+  action as rootAction 
+} from "./routes/root";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
